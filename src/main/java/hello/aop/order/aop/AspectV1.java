c3c615f32305c9e009fd31e9c08afecd41020e7f
cmong0516 <cmong0516@gmail.com>
2023-02-02 13:20:10 +0900

    2023.02.02

---
+++ src/main/java/hello/aop/order/aop/AspectV1.java
@@ -0,0 +1,18 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class AspectV1 {
+    // @Around 로 포인트컷 지정 hello.aop.order 하위의 클래스
+    // *(..) 모든 메서드
+    @Around("execution(* hello.aop.order..*(..))")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+}
---
+++ src/main/java/hello/aop/order/aop/AspectV2.java
@@ -0,0 +1,29 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+
+    // 포인트컷을 따로 분리할수 있다.
+
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    //pointcut expression private void allOrder(){} //pointcut signature
+    private void allOrder() {
+
+    }
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature());
+        return joinPoint.proceed();
+    }
+
+
+}
