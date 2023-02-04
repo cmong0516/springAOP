13078110879a3e4f0d7f7a4ef95971edeef11877
cmong0516 <cmong0516@gmail.com>
2023-02-04 09:27:32 +0900

    2023.02.04

--- src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -101,6 +101,7 @@ public class ExecutionTest {
     // .. 은 해당 위치의 패키지와 그 하위 패키지 포함d
 
     // 타입 매칭
+    // 부모 타입 허용
     @Test
     void typeExactMatch() {
         pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.*(..))");
@@ -113,4 +114,66 @@ public class ExecutionTest {
         Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
+    // 타입 매칭
+    // 부모 타입에 있는 메서드만 허용
+    @Test
+    void typeMatchInternal() throws NoSuchMethodException {
+        // hello.aop.member.MemberServiceImpl 의 모든 메서드
+        // MemberServiceImpl 에는 internal 메서드가 존재한다.
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.*(..))");
+
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+
+        Assertions.assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeMatchNoSuperTypeMethodFalse() throws NoSuchMethodException {
+        // hello.aop.member.MemberService 의 모든 메서드
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+
+        Assertions.assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isFalse();
+        // internal 메서드는 MemberServiceImpl 에는 있지만 MemberService 에는 없다.
+    }
+
+    // 파라미터 매칭
+
+    @Test
+    void argsMatch() {
+        pointcut.setExpression("execution(* *(String))");
+
+        // MemberServiceImpl 의 모든 메서드는 파라미터가 String 이다.
+
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void argsMatchNoArgs() {
+        // args 가 존재하지 않는것
+        pointcut.setExpression("execution(* *())");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void argsMatchStar() {
+        // args 가 단 하나인것
+        pointcut.setExpression("execution(* *(*))");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void argsMatchAll() {
+        // args 모두 허용. (없어도 OK)
+        pointcut.setExpression("execution(* *(..))");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void argsMatchComplex() {
+        // args 첫 인자가 String 타입.
+        pointcut.setExpression("execution(* *(String, ..))");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
 }
