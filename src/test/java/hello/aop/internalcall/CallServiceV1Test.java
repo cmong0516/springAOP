8d075a2fe2c72aff6590dc221e971190ebbbb68d
cmong0516 <cmong0516@gmail.com>
2023-02-11 05:32:02 +0900

    2023.02.11

--- src/test/java/hello/aop/internalcall/CallServiceV1Test.java
+++ src/test/java/hello/aop/internalcall/CallServiceV1Test.java
@@ -21,3 +21,6 @@ class CallServiceV1Test {
 
 // 순환참조는 SpringBoot 2.6 이후부터 금지되었다.
 // spring.main.allow-circular-references=true 를 추가해주어야 동작한다.
+
+// set 메서드를 통해 this 로 자동 호출되는 실제 객체가 아니라 프록시를 이용하여
+// log가 출력되었다.
